bba9cda4d20521339ae5e46c6eedd6f7fa365e3e
Elizabeth Le <elizabethlemk@gmail.com>
2021-05-21 22:23:26 -0400

    rendering with new state but nothing works

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 
 const Ticket = props => {
-    let { current_ticket, deselectCurrent, task, handleCurrentTicket, handleDragStart, handleEdit, handleDelete} = props;
+    let { current_ticket, deselectCurrent, task, handleCurrentTicket, handleDragStart, handleEdit, handleDelete, index, status} = props;
 
     const [display, setDisplay] = useState(false);
     const [value, setValue] = useState(task.value);
@@ -14,7 +14,7 @@ const Ticket = props => {
 
     const handleSubmit = (event, id) => {
         event.preventDefault();
-        handleEdit(id, value)
+        handleEdit(id, value, status)
         setDisplay(false)
         deselectCurrent()
     }
@@ -22,9 +22,9 @@ const Ticket = props => {
     return (
         <div draggable className="draggable"
             onClick={() => handleCurrentTicket(task.id)}
-            onDragStart={(e) => handleDragStart(e, task.name)}>
+            onDragStart={(e) => handleDragStart(e, task.name, index)}>
             
-            { display ?  (<form className="draggable__form" onSubmit={(e) => handleSubmit(e, task.id)}>
+            { display ? (<form className="draggable__form" onSubmit={(e) => handleSubmit(e, task.id, status)}>
                     <textarea onChange={handleChange} value={value} placeholder={task.name} ></textarea>
                     <button className="btn btn--primary" type="submit">Submit</button>
                 </form>) : task.name
@@ -32,7 +32,7 @@ const Ticket = props => {
 
             <div className="draggable__buttons" style={current_ticket === task.id && !display ? { display: "flex" } : { display: "none" }}>
                 <button className="btn btn--primary" onClick={() => setDisplay(true)}>Edit</button>
-                <button className="btn btn--link" onClick={e => handleDelete(e, task.id)}>Delete</button>
+                <button className="btn btn--link" onClick={e => handleDelete(e, task.id, status)}>Delete</button>
             </div>
         </div>
     )
